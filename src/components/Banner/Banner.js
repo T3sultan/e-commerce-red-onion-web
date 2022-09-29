0fd2ebc38327b0829182ea1c3faf6d956480701d
“T3sultan” <incognito.soul011@gmail.com>
2022-09-29 22:39:38 +0600

    image and banner and header

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -1,10 +1,34 @@
 import React from "react";
 import "./Banner.css";
+import backgroundImage from "../../assets/bannerbackground.png";
 
 const Banner = () => {
   return (
-    <div>
-      <h3>Banner</h3>
+    <div
+      className="grid justify-items-center items-center "
+      style={{
+        backgroundImage: `url(${backgroundImage})`,
+        width: "100%",
+        height: "400px",
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+        backgroundRepeat: "no - repeat",
+        marginTop: "10px",
+      }}
+    >
+      <div>
+        <h1 className="font-bold text-3xl text-gray-500  ">
+          Best food waiting for your belly
+        </h1>
+        <input
+          className="mt-5  w-3/5 h-8 b rounded-2xl "
+          type="text"
+          placeholder="  Search food items"
+        />{" "}
+        <button className="bg-rose-500 rounded-2xl w-1/4 h-8 text-white">
+          Search
+        </button>
+      </div>
     </div>
   );
 };
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,10 +1,21 @@
 import React from "react";
+import CustomLink from "../../common/CustomLink";
 import "./Header.css";
+import logo from "../../assets/logo2.png";
 
 const Header = () => {
   return (
-    <div>
-      <h3>Header</h3>
+    <div className="flex ml-10 mr-10 mt-5 header-container">
+      <img className="w-1/12 logo-container" src={logo} alt="" />
+      <div>
+        <nav className="flex gap-7">
+          <CustomLink to="/">Home</CustomLink>
+          <CustomLink className="auth-container " to="/login">
+            Login
+          </CustomLink>
+          <CustomLink to="/signup">Signup</CustomLink>
+        </nav>
+      </div>
     </div>
   );
 };
